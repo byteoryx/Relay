e17bc1c938fd4d346f915105042725fdd8973718
Andrii <andr53shr@gmail.com>
2024-06-09 21:02:45 +0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,13 +1,14 @@
 SHUFFLE_WALLETS = True  # True | False - перемешивать ли кошельки
 RETRY = 3  # кол-во попыток при ошибках / фейлах
 
-ETH_MAX_GWEI = 40
-
+ETH_MAX_GWEI = 30
 GWEI_MULTIPLIER = 1.05  # умножать текущий гвей при отправке транз на 5%
 
 TO_WAIT_TX = 1  # сколько минут ожидать транзакцию. если транза будет находится в пендинге после указанного времени то будет считатся зафейленной
-SLEEP_AFTER_TX = [10, 20]  # спать после каждой транзы от 10 до 20 секунд
+SLEEP_AFTER_TX = [15, 25]  # спать после каждой транзы от 10 до 20 секунд
 SLEEP_AFTER_ACCOUNT = [10, 20]  # спать после каждого аккаунта от 10 до 20 секунд
+
+# -------------------------------------------
 
 RPCS = {
     'ethereum': 'https://rpc.ankr.com/eth',
@@ -18,19 +19,16 @@ RPCS = {
     "zora": "https://rpc.zora.energy",
 }
 
+# -------------------------------------------
+
 # OKX
-MIN_ETH_BALANCE = 0.011  # если баланс в Ethereum меньше указанного - выводит в рандом сеть с OKX и бриджит в эфир
-WITHDRAWAL_CHAIN = [  # выводить с OKX в любую рандом сеть из указанных
+RELAY_BRIDGE_DESTINATION = "zora" # В какую конечную сеть делать вывод (erc20, zora)
+MIN_ETH_BALANCE = 0.1  # если баланс в Ethereum/Zora меньше указанного - выводит в рандом сеть с OKX и бриджит в эту сеть (если указать 0, то проверять баланс не будет)
+WITHDRAWAL_CHAIN = [  # выводит с OKX в любую рандом сеть из указанных (arbitrum, optimism, base, zksync) (для Zora сеть zksync убрать)
     "arbitrum",
     "optimism",
-    "base",
-    "zksync",
 ]
-OKX_WITHDRAW_VALUES = [0.012, 0.015]  # выводить в рандом сеть от 0.012 ETH до 0.015 ETH
-
-# CLUSTERS
-REF_CODE = "promintoff"  # реферальный код для минта Clusters
-OWN_DOMAINS = False  # False - генерировать случайные названия доменов | True - использовать свои указанные названия доменов из `own_domains.txt`
+OKX_WITHDRAW_VALUES = [0.002, 0.0025]  # выводить ETH в рандом сеть от и до 
 
 # -------------------------------------------
 
@@ -38,11 +36,15 @@ OKX_API_KEY = ""
 OKX_API_SECRET = ""
 OKX_API_PASSWORD = ""
 
-PROXY = 'http://log:pass@ip:port'  # что бы не использовать прокси - оставьте как есть
-CHANGE_IP_LINK = 'https://changeip.mobileproxy.space/?proxy_key=...&format=json'
+PROXY = ''  # proxy (http://)
+CHANGE_IP_LINK = '' # ссылка для смены IP
 
 TG_BOT_TOKEN = ''  # токен от тг бота (`12345:Abcde`) для уведомлений. если не нужно - оставляй пустым
 TG_USER_ID = []  # тг айди куда должны приходить уведомления. [21957123] - для отправления уведомления только себе, [21957123, 103514123] - отправлять нескольким людями
 
-# В какую сеть делать вывод
-RELAY_BRIDGE_DESTINATION = "zora" # erc20, zora
+
+
+
+# CLUSTERS (Не используется)
+REF_CODE = "promintoff"  # Не используется
+OWN_DOMAINS = False  # Не используется
